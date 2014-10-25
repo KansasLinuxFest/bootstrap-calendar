355755fd5564acab52c024f73950d068606e3aac
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-10-25 16:04:34 -0500

    adding the start of the plugin

---
+++ __init__.py
@@ -0,0 +1,65 @@
+# this is a plugin for pelican
+import os.path
+import os
+#import pelican.contents
+from pelican import signals
+from pelican.utils import (copy, process_translations, mkdir_p, DateFormatter,
+                           FileStampDataCacher, python_2_unicode_compatible)
+import shutil
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+class Blah(object):
+    def __init__(self, source_path=None):
+        self.source_path = source_path
+
+
+FILES = [
+    "css/calendar.min.css"
+]
+
+def register(pelican):
+    """
+    now we register this plugin into pelica
+    """
+
+
+
+    #pelican.context['staticfiles'].append(sc)
+
+class StaticGenerator(object):
+    """
+    new class for going custom generation for this class
+    """
+    def __init__(self, pelican=None, path=None, theme=None, output_path=None, context=None, settings=None):
+        self.pelican=pelican
+        self.path = path
+        self.output_path = output_path
+        #signals.static_generator_init.send(self)
+
+    def generate_aticles(self, write):
+        signals.article_generator_write_article.send(self, content=article)
+
+    def generate_context(self):
+        print 'gen context'
+
+    def generate_output(self, writer):
+        print 'gen output'
+
+        curpath = os.path.dirname(__file__)
+
+        # copy all Static files
+        for sc in FILES:
+            basename = os.path.basename(sc)
+            source_path = os.path.join(curpath, sc)
+            save_as = os.path.join(self.output_path, sc)
+            logger.info('Going to Copy %s to %s', sc, save_as)
+            mkdir_p(os.path.dirname(save_as))
+            shutil.copy2(source_path, save_as)
+            logger.info('Copying %s to %s', sc, save_as)
+
+def get_generators(pel):
+    return StaticGenerator
+   
