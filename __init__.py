0c49c4d7dc3aeeed79610543d6ad58d3c3ee39eb
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-10-25 21:37:23 -0500

    update

--- __init__.py
+++ __init__.py
@@ -16,9 +16,18 @@ class Blah(object):
         self.source_path = source_path
 
 
-FILES = [
-    "css/calendar.min.css"
-]
+FILES = {
+    "css/calendar.min.css" : "css/calendar.min.css",
+    'components/bootstrap3/css/bootstrap.min.css' : 'css/bootstrap.css',
+    'index.html' : 'calendar.html',
+    'components/bootstrap3/css/bootstrap-theme.min.css' : 'css/bootstrap-theme.min.css',
+    "components/jquery/jquery.min.js" : "js/jquery.min.js" ,
+    "components/underscore/underscore-min.js": "js/underscore-min.js",
+    "components/bootstrap3/js/bootstrap.min.js" : "js/bootstrap.min.js",
+    "components/jstimezonedetect/jstz.min.js" : "jstz.min.js",
+    "js/calendar.js": "js/calendar.js",
+    "js/app.js": "js/app.js",
+}
 
 def register(pelican):
     """
@@ -51,10 +60,11 @@ class StaticGenerator(object):
         curpath = os.path.dirname(__file__)
 
         # copy all Static files
-        for sc in FILES:
+        for sc in FILES.keys():
+            target = FILES[sc]
             basename = os.path.basename(sc)
             source_path = os.path.join(curpath, sc)
-            save_as = os.path.join(self.output_path, sc)
+            save_as = os.path.join(self.output_path, target)
             logger.info('Going to Copy %s to %s', sc, save_as)
             mkdir_p(os.path.dirname(save_as))
             shutil.copy2(source_path, save_as)
